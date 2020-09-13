d8e6b4fa75573913d995fb47efe1e64fc6300cf5
teja465 <katukuri.raviteja465@gmail.com>
2020-09-13 18:29:47 +0530

    amazon-clone

---
+++ precache-manifest.f6ddeff288bc68ca6df0c6d8ab27bdf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aed2511ec58b269bb18e",
+    "url": "/static/css/main.5db11540.chunk.css"
+  },
+  {
+    "revision": "aed2511ec58b269bb18e",
+    "url": "/static/js/main.b3a295ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "78c4b369596c0f0f9612",
+    "url": "/static/js/2.4b6b653a.chunk.js"
+  },
+  {
+    "revision": "21ea75c3af0e0855fc45b98c0bfac0a6",
+    "url": "/index.html"
+  }
+];
